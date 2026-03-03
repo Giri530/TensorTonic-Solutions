1152173e8ce058ce7fc54383cb3d61a7cb38e4d7
Girinath V <girinath439@gmail.com>
2026-03-03 18:51:51 +0530

    Update solution for auc

--- auc/auc.py
+++ auc/auc.py
@@ -4,9 +4,9 @@ def auc(fpr, tpr):
     fpr=np.asarray(fpr)
     tpr=np.asarray(tpr)
     sorted_index=np.argsort(fpr)
-    fpr_sort=fpr[sorted_index]
+    fpr_Sort=fpr[sorted_index]
     tpr_sort=tpr[sorted_index]
-    width=fpr[1:]-fpr[:-1]
-    height=tpr[1:]+tpr[:-1]
-    auc=np.sum((width*height)/2)
+    width=fpr_Sort[1:]-fpr_Sort[:-1]
+    height=tpr_sort[1:]+tpr_sort[:-1]
+    auc=np.sum(width*height)/2
     return float(auc)
