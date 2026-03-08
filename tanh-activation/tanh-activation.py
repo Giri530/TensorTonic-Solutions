5d138464372312926394df7aec88632245bb9ef8
Girinath V <girinath439@gmail.com>
2026-03-08 11:01:10 +0530

    Update solution for tanh-activation

--- tanh-activation/tanh-activation.py
+++ tanh-activation/tanh-activation.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def tanh(x):
-    x=np.array(x, dtype=float)
-    minus_exp=(np.exp(x)-np.exp(-x))
-    plus_exp=(np.exp(x)+np.exp(-x))
+    x=np.asarray(x)
+    minus_exp=np.exp(x)-np.exp(-x)
+    plus_exp=np.exp(x)+np.exp(-x)
     return minus_exp/plus_exp
