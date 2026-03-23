42c4a4289af0ca7226c12a38566ec5b9185007c9
Girinath V <girinath439@gmail.com>
2026-03-23 11:31:32 +0530

    Add solution for causal-masking

---
+++ causal-masking/causal-masking.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def apply_causal_mask(scores, mask_value=-1e9):
+    scores=np.array(scores,dtype=float)
+    T=scores.shape[-1]
+    mask=np.triu(np.ones((T,T),dtype=bool),k=1)
+    masked_scores=np.where(mask,mask_value,scores)
+    return masked_scores
